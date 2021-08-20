1ec9fea4aa1775a6a2bd200967d6a0e0d8c4ba9d
sencha-tech <72788598+sencha-tech@users.noreply.github.com>
2021-08-20 21:10:57 +0530

    Update Flickr.py

--- Flickr.py
+++ Flickr.py
@@ -1,4 +1,4 @@
 Import requests
-Response = requests.get("https://
+Response = requests.get("https://www.flickr.comservices/rest/?method=flickr.photos.getPopular&api_key=fd70e4e90cc36ed3ecc1fe8319ace1f3&user_id=193746000@NO6&format=json&nojsoncallback=1")
 Print (response.json())
 Print(response.status_code)
